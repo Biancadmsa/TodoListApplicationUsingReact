c82602564920062d8f579b3489c8522eb7edb453
Bianca Salcedo <biancadmsadf16@gmail.com>
2023-10-03 22:10:56 +0000

    myfivecommit

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,26 +1,47 @@
 import React, { useState } from 'react';
 
 
+
+
+// [ ]
 const Home = () => {
-  return(
-<div className ="container"> 
-
-<h1>My TodoList</h1>
-    <ul>
-      <li><input type="text" placeholder="What needs to be done?" ></input>
-      </li>
-        <li>Make the bed <i class="fa-solid fa-xmark"></i>
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos]=useState([]);
+  return (
+  <div className ="container"> 
+  
+  <h1>My Todos</h1>
+  
+      <ul>
+        <li>
+          <input type="text" 
+          onChange={(e)=> setInputValue(e.target.value)} 
+          value={inputValue} 
+          onKeyDown= {(e) => {
+          
+            if (e.key === "Enter"){
+              setTodos(todos.concat(inputValue));
+              setInputValue("");
+            }
+         }}
+          
+          placeholder="What needs to be done?"></input>
         </li>
-        <li>Make the dog <i class="fa-solid fa-xmark"></i></li>
-        <li>Pay taxes <i class="fa-solid fa-xmark"></i></li>
-        <li>Go on Vacation <i class="fa-solid fa-xmark"></i></li>
-        <div>item left</div>
-        
-    </ul>
-</div>
-
-
-  );
-};
-
-export default Home;
+        {todos.map((item, index)=>(
+        <li>
+          {item}{""}<i class="fa-solid fa-xmark" 
+          onClick={()=>
+          setTodos(
+            todos.filter(
+            (t, currentIndex)=> 
+            index != currentIndex
+            )
+          )
+        }></i>
+        </li>
+      ))}
+      </ul>
+  <div>{todos.length} Item</div>
+  </div>
+  )}
+  export default Home;
